594892cb0da066c93fa02c298d688d7b5760eec9
McDermott <patrick@pjmcdermott.com>
2024-04-05 09:13:49 -0700

    finish initial preview of site

---
+++ src/testimonials.ts
@@ -0,0 +1,24 @@
+export const testimonials = [
+    {
+        name: 'Alex B',
+        pubDate: '2023-03-01',
+        testimony: `
+        <p>Matt showed up very shortly after I called about my AC not working. He is very friendly and walked me through what he was doing and why. And even showed me how to properly maintain my AC so I can prevent this issue from happening in the future.</p>
+        <p>He did it all for a great price too.</p>
+        `,
+    },
+    {
+        name: 'Andrew Desautels',
+        pubDate: '2024-02-02',
+        testimony: `
+        <p>Absolutely the best. Always immediate response, and personal dedication. I’m always able to call on them when my system betrays me!</p>
+        `,
+    },
+    {
+        name: 'Ben Andrews',
+        pubDate: '2024-02-02',
+        testimony: `
+        <p>Absolutely the best. Always immediate response, and personal dedication. I’m always able to call on them when my system betrays me!</p>
+        `,
+    },
+];
